a29c1a575f5fa7f21c6bb576229a15a11c76cbc8
nguyencamchau <nguyencamchau455@gmail.com>
2026-05-12 10:49:59 +0700

    Tao khung CT main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void nhap(int &n) {
+}
+void xuat(int n) {
+}
+void kieTraChan(int n){
+} 
+int main() {
+    int n;
+    nhap(n);
+    xuat(n);
+    kieTraChan(n);
+    return 0;
+}
